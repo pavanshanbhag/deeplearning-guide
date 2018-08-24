5144d040b917c3ba523d00c4aa030b541d2890bb
pavanshanbhag <11908156+pavanshanbhag@users.noreply.github.com>
2018-08-24 09:25:38 +0530

    Linear Regression with Gradient Descent

--- code/linear-regression-demo/demo1.py
+++ code/linear-regression-demo/demo1.py
@@ -1,3 +1,19 @@
+"""
+linear_regression_demo
+
+Overview
+The goal is to predict an animal's body weight given it's brain weight. 
+The model we'll be using is called [Linear Regression](http://www.statisticssolutions.com/what-is-linear-regression/). 
+The dataset we're using to train our model is a list of brain weight and body weight
+measurements from a bunch of animals. We'll fit our line to the data using the 
+scikit learn machine learning library, then plot our graph using matplotlib.
+
+Dependencies
+* pandas
+* scikit-learn
+* matplotlib
+"""
+
 import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
---
+++ code/linear-regression-demo/demo2.py
@@ -0,0 +1,77 @@
+"""
+"Linear Regression the Right Way" 
+
+Overview
+Using a small dataset of student test scores and the amount of hours they studied. Intuitively, there must be a relationship right? The more you study, the better your test scores should be. We're going to use linear regression to prove this relationship.
+
+Some helpful links:
+
+Gradient descent visualization
+https://raw.githubusercontent.com/mattnedrich/GradientDescentExample/master/gradient_descent_example.gif
+
+Sum of squared distances formula (to calculate our error)
+https://spin.atomicobject.com/wp-content/uploads/linear_regression_error1.png
+
+Partial derivative with respect to b and m (to perform gradient descent)
+https://spin.atomicobject.com/wp-content/uploads/linear_regression_gradient1.png
+
+Dependencies
+numpy
+
+More learning resources:
+https://spin.atomicobject.com/2014/06/24/gradient-descent-linear-regression/
+https://www.analyticsvidhya.com/blog/2017/03/introduction-to-gradient-descent-algorithm-along-its-variants/
+https://en.wikipedia.org/wiki/Gradient_descent
+https://machinelearningmastery.com/gradient-descent-for-machine-learning/
+http://ufldl.stanford.edu/tutorial/supervised/OptimizationStochasticGradientDescent/
+
+"""
+
+#The optimal values of m and b can be actually calculated with way less effort than doing a linear regression. 
+#this is just to demonstrate gradient descent
+
+from numpy import *
+
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run():
+    points = genfromtxt(r"datasets\demo2_data.csv", delimiter=",")
+    learning_rate = 0.0001
+    initial_b = 0 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    num_iterations = 1000
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Running...")
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    print ("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
+
+if __name__ == '__main__':
+    run()
